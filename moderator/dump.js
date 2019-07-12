4617e3e6c5de6cd911d625cc657e05790d965448
wangshijun <wangshijun2010@gmail.com>
2019-07-13 07:25:22 +0800

    fix: moderator dump script

--- moderator/dump.js
+++ moderator/dump.js
@@ -7,4 +7,6 @@ const moderator = ForgeSDK.Wallet.fromSecretKey(sk);
 const json = moderator.toJSON();
 
 console.log('moderator wallet', JSON.stringify(json, true, '  '));
-console.log(`moderator pk base64: ${base64.escape(base64.encode(hexToBytes(json.pk)))}`);
+console.log(
+  `moderator pk base64: ${base64.escape(base64.encode(ForgeSDK.Util.hexToBytes(json.pk)))}`
+);
