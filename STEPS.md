60935dfe50047565d012a426a4445bd72a68afdd
wangshijun <wangshijun2010@gmail.com>
2019-07-13 08:28:59 +0800

    add steps.md and fund_dapp script

---
+++ STEPS.md
@@ -0,0 +1,34 @@
+## How to setup?
+
+- forge cli
+- forge install (china mirror)
+- forge config
+- configuration detail
+  - dump moderator
+  - transfer from moderator [token]
+- read data with forge-cli
+
+## Generate some transactions?
+
+- run examples, written with javascript sdk
+
+## Forge Web
+
+- forge web open
+  - dashboard
+  - explorer
+  - protocols
+  - graphiql playground
+
+## Creating dApp
+
+- create the dapp with `forge create-project`
+  - forge dapp starters
+- fund the dapp from moderator
+- lucky money logic
+- test with dapp
+
+## Join a chain
+
+- forge join
+- peers map
--- moderator/fund_dapp.js
+++ moderator/fund_dapp.js
@@ -6,27 +6,16 @@ ForgeSDK.connect('http://127.0.0.1:8210/api');
 (async () => {
   const sk = ensureModeratorSecretKey();
   const moderator = ForgeSDK.Wallet.fromSecretKey(sk);
-  const receiver = ForgeSDK.Wallet.fromRandom();
-  console.log('receiver', JSON.stringify(receiver.toJSON(), true, '  '));
 
-  // Declare the receiver
-  const hash = await ForgeSDK.sendDeclareTx({
-    tx: {
-      itx: { moniker: 'receiver' },
-    },
-    wallet: receiver,
-  });
-  console.log(`receiver declare hash ${hash}`);
-
-  // Transfer to receiver
+  // Transfer to application
   const hash2 = await ForgeSDK.sendTransferTx({
     tx: {
       itx: {
-        to: receiver.toAddress(),
+        to: process.env.DAPP_ID,
         value: ForgeSDK.Util.fromTokenToUnit(10000),
       },
     },
     wallet: moderator,
   });
-  console.log(`receiver transfer hash ${hash2}`);
+  console.log(`application funded: ${hash2}`);
 })();
